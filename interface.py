199784599b9b2f25e0818c96e9a2f454a22f3230
nancywang197 <108085245+nancywang197@users.noreply.github.com>
2024-02-24 14:25:20 -0600

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,14 +5,15 @@ import numpy as np
 model = tf.keras.models.load_model('handwritten.model')
 
 def recognize_character(image):
-    img = np.invert(np.array([image]))
+    img_resize = np.resize(image['composite'], (200,200,3))
+    img = np.invert(np.array([img_resize]))
     prediction = model.predict(img)
     return np.argmax(prediction)
     
 
 interface = gr.Interface(fn=recognize_character, 
-                         inputs=gr.Sketchpad(height = 200, width=200),
-                         outputs="label",
+                         inputs=gr.Sketchpad(height = 600, width=800, image_mode='RGB'),
+                         outputs="text",
                          live=True)
 
 interface.launch()
